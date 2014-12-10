a5aff7fe86b9aef4488ff7da37ae7f6df75ada7b
Cymen Vig <cymenvig@gmail.com>
2014-12-10 11:38:21 -0800

    Initial commit - works

--- built/common.js
+++ built/common.js
@@ -1,11 +1,13 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var robot = require('./robot');
 
-console.log(robot(42));
+console.log(robot('42'));
 
-},{"./robot":undefined}],"/robot.js":[function(require,module,exports){
+},{"./robot":"9WgEjc"}],"9WgEjc":[function(require,module,exports){
 module.exports = function (s) {
   return s.toUpperCase() + '!'
 };
 
-},{}]},{},[1]);
+},{}],"./robot.js":[function(require,module,exports){
+module.exports=require('9WgEjc');
+},{}]},{},[1])
